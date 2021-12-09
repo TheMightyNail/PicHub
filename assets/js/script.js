6a9b4dbcdf35a8e732c80ae4c6c885fbe0df6bc7
Ian Linton <ilinton2571@gmail.com>
2021-12-08 21:05:06 -0700

    began work on initial javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,31 @@
 // Pexels API authorization
-import { createClient } from 'pexels';
+var apiKey= "563492ad6f9170000100000164255fc255754e7795509040e702d9aa";
+var apiURL="https://api.pexels.com/v1/";
 
-const client = createClient('563492ad6f9170000100000164255fc255754e7795509040e702d9aa');
+var inputEl= $("submit");
+var searchButtonEl = $("button");
+
+var searchText ="";
+// var input = ""
+
+// create a function that when the user inputs text and hits Enter, it will perform the same function as when clicking the search button
+var submitSearch = function() {
+    $("input").on("input", function(event){
+        event.preventDefault();
+        search_text=event.target.value;
+        console.log(search_text);
+    });
+}
+
+
+
+// event listener for button clicks
+$("button").on("click", function(){
+    console.log("This is a button");
+    console.log(inputEl)
+});
+
+
 
 // When user submits input in the search bar, dynamically generate images based on the search query
 // Reference Pexels for image search queries
