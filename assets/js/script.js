bffe3ab23e4a250f85abf1932018ab818518677a
Ian Linton <ilinton2571@gmail.com>
2021-12-08 19:42:24 -0700

    added SwiperJS script and CSS links

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,10 @@
 import { createClient } from 'pexels';
 
 const client = createClient('563492ad6f9170000100000164255fc255754e7795509040e702d9aa');
+
+// When user submits input in the search bar, dynamically generate images based on the search query
+// Reference Pexels for image search queries
+// SwiperJS code to create Swiper rows for images
+// Use slides per view to limit the amount of images displayed at once, maybe?
+// How many images can the page load at once?
+// utilize localStorage for persistence - previously searched list by that user, or previously viewed images?
