afbd3d5cb1acafed89ae05e89c0a7b3e41357cd7
Christian-Henriquez <chris-henriquez@outlook.comgit config --global user.name Christian-Henriquez>
2021-12-10 18:47:37 -0700

    removed fetch command from html and scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,7 @@ import { createClient } from 'pexels';
 
 const client = createClient('563492ad6f9170000100000164255fc255754e7795509040e702d9aa');
 
-{ 
-    function fetch (https://api.pexels.com/v1/search/q=dogs).then(response => response.json).then(data => {for (i<0; i<data.length; i++){document.querySelector(#parent-of-images).append = <img src={data[0].url/>}}
-    
-}
+
 
 // When user submits input in the search bar, dynamically generate images based on the search query
 // Reference Pexels for image search queries
